ba8cb0df429b294ce93f1626d8f404b37f3ef2d8
abderraouf-metidji <115145685+abderraouf-metidji@users.noreply.github.com>
2024-09-09 10:33:38 +0200

    code cleaning

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from ants import Fourmiliere, lire_fichier_fourmiliere, visualiser_deplacements
 
-if __name__ == "__main__":
+def main():
+    """
+    Exécute la simulation de la fourmilière en fonction du fichier sélectionné par l'utilisateur.
+    """
     fichier = input("Entrer le numéro du fichier de fourmilière à simuler (0, 1, 2, 3, 4, 5): ")
     fichiers = {
         '0': 'Nest/fourmiliere_zero.txt',
@@ -11,18 +14,16 @@ if __name__ == "__main__":
         '5': 'Nest/fourmiliere_cinq.txt'
     }
     
-    # Vérification que l'entrée utilisateur est correcte
     if fichier not in fichiers:
         print(f"Erreur : le numéro de fichier '{fichier}' est invalide. Veuillez entrer un numéro entre 0 et 5.")
     else:
-        fichier = fichiers[fichier]  # Sélection du fichier correspondant
-
+        fichier = fichiers[fichier]
         nb_fourmis, salles, tunnels, capacites = lire_fichier_fourmiliere(fichier)
-
         fourmiliere = Fourmiliere(salles, tunnels, nb_fourmis, capacites)
         etapes = fourmiliere.simuler()
-
+        
         print(f"Nombre d'étapes pour que chaque fourmi arrive au dortoir: {etapes}")
-
-        # Visualiser les déplacements des fourmis
         visualiser_deplacements(fourmiliere)
+
+if __name__ == "__main__":
+    main()
